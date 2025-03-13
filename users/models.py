10af5a5e9a1fae750b8d9ab5ca3e90e3faa0e6b7
mexic <mexic2284@mail.ru>
2025-03-13 18:07:04 +0400

    Реализована модель User и модель Profile(доп инфо о модели User)

--- users/models.py
+++ users/models.py
@@ -1,6 +1,35 @@
+from authemail.models import EmailAbstractUser
 from django.db import models
-from django.contrib.auth import base_user
+from phonenumber_field.modelfields import PhoneNumberField
 
 
-class BaseMode(models.Model):
-    title = models.CharField()
+class User(EmailAbstractUser):
+    first_name = models.CharField(max_length=30, null=True, blank=True, verbose_name="Имя пользователя")
+    last_name = models.CharField(max_length=30, null=True, blank=True, verbose_name="Фамилия пользователя")
+    date_joined = models.DateTimeField(auto_now_add=True, verbose_name="Дата регистрации")
+    last_login = models.DateTimeField(auto_now=True, verbose_name="Время последнего входа")
+    date_commencement = models.DateTimeField(null=False, verbose_name="Стаж работы")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_commencement']
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    phone = PhoneNumberField(max_length=20, null=True, blank=True, verbose_name="Телефон")
+    image = models.ImageField(upload_to='profile_images/', null=True, blank=True, verbose_name="Аватар")
+    date_birthday = models.DateField(null=True, blank=True, verbose_name="Дата рождения")
+
+    class Meta:
+        verbose_name = "Пользователь инфо"
+        verbose_name_plural = "Пользователи инфо"
+
+    def __str__(self):
+        return f"Profile of {self.user.email}"
