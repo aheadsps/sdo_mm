a0ed5661e8bb6c597592788e03891780221fa850
mexic <mexic2284@mail.ru>
2025-03-13 18:14:06 +0400

    fix

--- users/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from users.models import BaseMode
-
-
-class BaseModeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BaseMode
-        fields = '__all__'
--- users/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-from users.serializers import BaseModeSerializer
-from users.models import BaseMode
-
-
-class CreateBaseMode(generics.CreateAPIView):
-    serializer_class = BaseModeSerializer
-    queryset = BaseMode.objects.get_queryset()
