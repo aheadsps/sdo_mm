2faf15e7d2eed3b83cd588ed0294fccd6d635449
mexic <mexic2284@mail.ru>
2025-03-15 00:02:29 +0400

    Добавлена форма CustomUserCreationForm для создания пользователя в users/admin.py Пароль генерируется автоматически и отправляется на email. Указаны обязательные поля при создании пользователя (email и date_commencement).

--- users/admin.py
+++ users/admin.py
@@ -2,35 +2,56 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from authemail.admin import EmailUserAdmin, SignupCodeInline, EmailChangeCodeInline, PasswordResetCodeInline
 
+from users.forms import CustomUserCreationForm
 from users.models import Profile
 
 
 class ProfileInline(admin.StackedInline):
-	model = Profile
-	can_delete = False
-	verbose_name_plural = 'Profile'
-	fk_name = 'user'
+	"""
+	Определяем встроенный класс для отображения модели Profile в админке у User
+	"""
+	model = Profile  # Указываем модель, которую будем встраивать
+	can_delete = False  # Запрещаем удаление профиля из админки пользователя
+	verbose_name_plural = 'Профиль инфо'  # Название для отображения в админке
+	fk_name = 'user'   # Указываем поле, которое связывает Profile с User
 
 
 class UserAdmin(EmailUserAdmin):
-	inlines = ( ProfileInline, SignupCodeInline, EmailChangeCodeInline, PasswordResetCodeInline,)
+	"""
+	Кастомная админка для модели пользователя.
+	Использует форму CustomUserCreationForm для создания пользователей.
+	Пароль генерируется автоматически и отправляется на почту, нужно ввести только email
+	"""
+	add_form = CustomUserCreationForm  # Используем кастомную форму
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': ('email',  'date_commencement'),  # Указываем только то что обязательны при заполнении
+		}),
+	)
+	# Встраиваемые модели
+	inlines = (ProfileInline, SignupCodeInline, EmailChangeCodeInline, PasswordResetCodeInline,)
 	fieldsets = (
 		(None, {'fields': ('email', 'password')}),
 		('Personal Info', {'fields': ('first_name', 'last_name')}),
+		('Important dates', {'fields': ('date_joined', 'last_login')}),
 		('Permissions', {'fields': ('is_active', 'is_staff',
 									'is_superuser', 'is_verified',
 									'groups', 'user_permissions')}),
-		('Important dates', {'fields': ('date_joined', 'last_login')}),
 	)
+
 	readonly_fields = ('date_joined', 'last_login')
 
 
+# Отменяем регистрацию стандартной модели пользователя
 admin.site.unregister(get_user_model())
+# Регистрируем кастомную админку для модели пользователя
 admin.site.register(get_user_model(), UserAdmin)
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+	""" Регистрация модели Profile в админке. """
 	list_display = ('user', 'phone', 'date_birthday')
 	list_filter = ('date_birthday',)
 	search_fields = ('user__email', 'phone')
