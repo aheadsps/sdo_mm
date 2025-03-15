8e0b4bf4e2533825b62f562533bdcf460815269b
Alex Pavlov <yukiu217@gmail.com>
2025-03-16 01:23:50 +0600

    add admin panels for models

--- lessons/admin.py
+++ lessons/admin.py
@@ -8,3 +8,11 @@ class AnswerAdmin(admin.ModelAdmin):
     Админ панель Ответа
     """
     list_display = ('text', 'correct', 'question')
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    Админ панель Вопроса
+    """
+    list_display = ('text', 'image')
