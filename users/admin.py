cc84b5e6a2646fcbc6e172980b5b0ed62a32a272
Serg_Nout <kuchukov.sergey@gmail.com>
2025-03-15 10:35:05 +0300

    При создании пользователя (админ панель) добавлять  пользователя в группу профессии

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from authemail.admin import EmailUserAdmin, SignupCodeInline, EmailChangeCodeInline, PasswordResetCodeInline
-
-from users.models import Profile
+from users.forms import CastomEmailUserChangeForm
+from users.models import Profile, Profession
 
 
 class ProfileInline(admin.StackedInline):
@@ -15,14 +15,21 @@ class ProfileInline(admin.StackedInline):
 class UserAdmin(EmailUserAdmin):
 	inlines = ( ProfileInline, SignupCodeInline, EmailChangeCodeInline, PasswordResetCodeInline,)
 	fieldsets = (
-		(None, {'fields': ('email', 'password')}),
+		(None, {'fields': ('email', 'password', 'date_commencement', 'profession')}),
 		('Personal Info', {'fields': ('first_name', 'last_name')}),
 		('Permissions', {'fields': ('is_active', 'is_staff',
 									'is_superuser', 'is_verified',
 									'groups', 'user_permissions')}),
 		('Important dates', {'fields': ('date_joined', 'last_login')}),
 	)
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': ('email', 'password1', 'password2',  'date_commencement', 'profession'),
+		}),
+	)
 	readonly_fields = ('date_joined', 'last_login')
+	form = CastomEmailUserChangeForm
 
 
 admin.site.unregister(get_user_model())
@@ -31,7 +38,7 @@ admin.site.register(get_user_model(), UserAdmin)
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-	list_display = ('user', 'phone', 'date_birthday')
+	list_display = ('user',  'phone', 'date_birthday')
 	list_filter = ('date_birthday',)
 	search_fields = ('user__email', 'phone')
 	raw_id_fields = ('user',)
@@ -41,3 +48,8 @@ class ProfileAdmin(admin.ModelAdmin):
 		('Contact Info', {'fields': ('phone',)}),
 		('Personal Info', {'fields': ('image', 'date_birthday')}),
 	)
+
+@admin.register(Profession)
+class ProfessionAdmin(admin.ModelAdmin):
+	list_display = ('en_name', 'ru_name')
+	search_fields = ('en_name', 'ru_name')
