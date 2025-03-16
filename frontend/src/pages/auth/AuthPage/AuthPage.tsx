b700688b51bc3bdbfdf65049d9a01546f79431bd
Kholov Rustam <rustamkholow5@gmail.com>
2025-03-16 18:56:44 +0300

    checkpoint

---
+++ frontend/src/pages/auth/AuthPage/AuthPage.tsx
@@ -0,0 +1,9 @@
+const AuthPage: React.FC = () => {
+  return (
+    <>
+    <
+    </>
+  )
+}
+
+export default AuthPage
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,10 +2,17 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { Auth } from '@pages/auth'
 
+import AuthForm from '../src/features/AuthForm/AuthForm'
+import AuthPage from '@pages/auth/AuthPage/AuthPage'
+
 export const router = createBrowserRouter([
   {
     path: '/auth',
     element: <Auth />,
+  },
+  {
+    path: '/authform',
+    element: <AuthPage />,
   },
   {
     path: '/',
