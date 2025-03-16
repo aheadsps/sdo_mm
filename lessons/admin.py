161b37d8b95b199da439a65be0c610d4dba5c335
Serg_Nout <kuchukov.sergey@gmail.com>
2025-03-16 20:47:19 +0300

    lesson

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from lessons.models import Step
 # Register your models here.
+
+@admin.register(Step)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+        "serial",
+        "title",
+    )
+    search_fields = (
+        "title",
+    )
--- lessons/models.py
+++ lessons/models.py
@@ -1,31 +1,26 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
-class IntegerRangeField(models.IntegerField):
-    """ класс поля модели IntegerField с ограничением по макс и мин заначению"""
-
-    def __init__(self, min_value=None, max_value=None, **kwargs):
-        self.validators = [MinValueValidator(min_value),
-                           MaxValueValidator(max_value)]
-        super().__init__(**kwargs)
 
 class Step(models.Model):
     """
     Шаги урока
     """
     title = models.CharField(max_length=256, null=False, blank=False, verbose_name="Шаг урока")
-    content_text = models.TextField(verbose_name="Контент шага урока")
-    serial = IntegerRangeField(null=False,
+    content_text = models.TextField(verbose_name="Контент шага урока",
+                                    null=True,
+                                    blank=True,
+                                    )
+    serial = models.IntegerField(null=False,
                                  blank=False,
-                                 min_value=1,
                                  default=1,
+                                 validators=[MinValueValidator(1)],
                                  verbose_name="Порядковый номер шага"
                               )
     # media_attachment Медиа вложения. (Только в логике сериализатора, в модели не реализуем!)
 
     # Тестовый блок для этого шага #/components/schemas/TestBlock
-    # test_block = models.OneToOneField(TestBlock, on_delete=models.CASCADE, related_name='step')
+    # test_block = models.OneToOneField(TestBlock, on_delete=models.CASCADE, related_name='steps')
 
     class Meta:
         verbose_name = "Шаг урока"
