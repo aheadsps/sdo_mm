4ad64acc0a9b197309abb1bc8b65a314a62d358a
Alex Pavlov <yukiu217@gmail.com>
2025-03-17 22:51:25 +0600

    add custom viewset

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets, permissions
 
-# Create your views here.
+from lessons import models
+from lessons import serializers
+from lessons import viewsets as own_viewsets
+from lessons.permissions import IsAdminOrIsStaff
+
+
+class EventViewSet(own_viewsets.GetUpdateDeleteViewSet):
+    """
+    Виювсет эвента
+    """
+    models = models.Event._default_manager.get_queryset()
+    serializers = serializers.EventSerializer
+
+    def get_permissions(self):
+        if not self.action == 'retrieve':
+            permission_classes = [IsAdminOrIsStaff]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
