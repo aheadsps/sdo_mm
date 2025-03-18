686e85a12a9a12626416e5840d74dfb12cc789a5
Serg_Nout <kuchukov.sergey@gmail.com>
2025-03-18 07:24:57 +0300

    views

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.generics import (CreateAPIView,
+                                     ListAPIView,
+                                     RetrieveAPIView,
+                                     UpdateAPIView,
+                                     DestroyAPIView
+                                     )
+from lessons.serializer import StepSerializer
+from lessons.models import Step
+
 
 # Create your views here.
+class StepCreateViewSet(CreateAPIView):
+    queryset = Step.objects.all()
+    serializer_class = StepSerializer
+
+
+class StepListViewSet(ListAPIView):
+        queryset = Step.objects.all()
+        serializer_class = StepSerializer
+
+
+class StepDetailViewSet(RetrieveAPIView):
+        queryset = Step.objects.all()
+        serializer_class = StepSerializer
+
+
+class StepUpdateViewSet(UpdateAPIView):
+        queryset = Step.objects.all()
+        serializer_class = StepSerializer
+
+
+class StepDeleteViewSet(DestroyAPIView):
+        queryset = Step.objects.all()
+        serializer_class = StepSerializer
