f6267028c60f595d5971d63cbfce6d4e6a23df2c
Serg_Nout <kuchukov.sergey@gmail.com>
2025-03-18 08:33:01 +0300

    fixed

--- lessons/models.py
+++ lessons/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from django.utils.translation import gettext_lazy as _
 
 from lessons.utils import path_maker_text
--- lessons/views.py
+++ lessons/views.py
@@ -1,11 +1,4 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import (CreateAPIView,
-                                     ListAPIView,
-                                     RetrieveAPIView,
-                                     UpdateAPIView,
-                                     DestroyAPIView
-                                     )
 from lessons.serializer import StepSerializer
 from lessons.models import Step
 
@@ -14,28 +7,3 @@ from lessons.models import Step
 class StepViewSet(ModelViewSet):
     queryset = Step.objects.all()
     serializer_class = StepSerializer
-
-
-"""class StepCreateViewSet(CreateAPIView):
-    queryset = Step.objects.all()
-    serializer_class = StepSerializer
-
-
-class StepListViewSet(ListAPIView):
-        queryset = Step.objects.all()
-        serializer_class = StepSerializer
-
-
-class StepDetailViewSet(RetrieveAPIView):
-        queryset = Step.objects.all()
-        serializer_class = StepSerializer
-
-
-class StepUpdateViewSet(UpdateAPIView):
-        queryset = Step.objects.all()
-        serializer_class = StepSerializer
-
-
-class StepDeleteViewSet(DestroyAPIView):
-        queryset = Step.objects.all()
-        serializer_class = StepSerializer"""
