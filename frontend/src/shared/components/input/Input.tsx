c57b80be2d1a5280fabbdb1295729e4f64b4473b
Kholov Rustam <rustamkholow5@gmail.com>
2025-03-19 21:03:42 +0300

    input was channged

--- frontend/src/shared/components/input/Input.tsx
+++ frontend/src/shared/components/input/Input.tsx
@@ -1,25 +1,12 @@
 import clsx from 'clsx'
-import { ComponentPropsWithRef, ElementType, forwardRef } from 'react'
+import { ComponentPropsWithRef, forwardRef } from 'react'
 
 import s from './input.module.scss'
 
-type Props<T extends ElementType = 'input'> = {
-  variant?: 'primary' | 'secondary'
-  as?: T
-} & ComponentPropsWithRef<T>
+type Props = ComponentPropsWithRef<'input'>
 
-export const Input = forwardRef<HTMLInputElement, Props>(
-  ({ variant = 'primary', as: Component = 'input', className, ...restProps }, ref) => {
-    const chosenInput = variant === 'secondary' ? s.secondary : s.primary
-
-    return (
-      <Component
-        ref={ref}
-        className={clsx(s.input, chosenInput, className as string)}
-        {...restProps}
-      />
-    )
-  }
-)
+export const Input = forwardRef<HTMLInputElement, Props>(({ className, ...restProps }, ref) => {
+  return <input ref={ref} className={clsx(s.input, className)} {...restProps} />
+})
 
 Input.displayName = 'Input'
