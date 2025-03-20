6162395851bd175ffdd40d9d9bc775ce05bd798b
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-03-20 17:51:49 +0300

    права доступа...

--- lessons/tests/test_url_step.py
+++ lessons/tests/test_url_step.py
@@ -1,12 +1,60 @@
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from lessons.models import Step
 from rest_framework import status
-
+from users import models as users_models
+import datetime
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 class TestStepUrl(APITestCase):
     """
     Тесты основных функций модели Step
     """
+
+    def setUp(self):
+        """
+        Подключение пользователя
+        """
+        self.profession = users_models.Profession._default_manager.create(
+            en_name="prof",
+            ru_name="проф",
+        )
+        date_commencement = datetime.date(
+            year=2023,
+            month=1,
+            day=1,
+        )
+        self.user = get_user_model()._default_manager.create(
+            email="user@gmail.com",
+            profession=self.profession,
+            password="password",
+            date_commencement=date_commencement,
+        )
+        self.user.set_password("password")
+        self.user.save()
+        group_profession = users_models.ProfessionGroup._default_manager.create(
+            profession=self.profession,
+        )
+        group_profession.students.add(self.user)
+        group_profession.save()
+        # добавить в группу модераторов
+        Group.objects.create(name='admin')
+        group = Group.objects.create(name='methodist')
+        group.user_set.add(self.user)
+        ct = ContentType.objects.get_for_model(Step)
+        permission = Permission.objects.create(
+            codename="can_change_step",
+            name="Модерировать Step",
+            content_type=ct,
+        )
+        #group.permissions.add(permission)
+        group.save()
+
+        self.client = APIClient()
+        self.client.login(username='user@gmail.com', password='password')
+        self.client.force_authenticate(user=self.user)
+
 
     def test_step_url_create(self):
         """
@@ -25,8 +73,8 @@ class TestStepUrl(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        self.assertEqual(Step.objects.count(), 1)
-        step = Step.objects.get()
+        self.assertEqual(Step._default_manager.count(), 1)
+        step = Step._default_manager.get()
         self.assertEqual(step.title, 'Шаг 4')
 
         """
@@ -44,7 +92,7 @@ class TestStepUrl(APITestCase):
         }
         response = self.client.patch(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Step.objects.get().content_text, 'Новый текст')
+        self.assertEqual(Step._default_manager.get().content_text, 'Новый текст')
 
         """
         Выводим один объект Step
@@ -66,3 +114,9 @@ class TestStepUrl(APITestCase):
         url = '/step/' + str(step.id) + '/'
         response = self.client.delete(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
--- lessons/views.py
+++ lessons/views.py
@@ -1,21 +1,41 @@
 from rest_framework.viewsets import ModelViewSet, GenericViewSet, mixins
 from lessons.serializers import StepSerializer
 from lessons.models import Step
-from django.contrib.auth.mixins import (LoginRequiredMixin,
-                                        PermissionRequiredMixin)
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin,)
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
-class StepViewSet( ModelViewSet ):
-    queryset = Step.objects.all()
+class StepViewSet(LoginRequiredMixin,
+                  #PermissionRequiredMixin,
+                  ModelViewSet ):
+    queryset = Step._default_manager.all()
+    serializer_class = StepSerializer
+    #permission_required = "lessons.can_change_step"
+
+class StepDetailViewSet(LoginRequiredMixin,
+                        ModelViewSet
+                        ):
+    queryset = Step._default_manager.all()
     serializer_class = StepSerializer
 
-class StepDetailViewSet( ModelViewSet ):
-    queryset = Step.objects.all()
-    serializer_class = StepSerializer
-
+    #permission_required = "lessons.can_change_step"
     def retrieve(self, request, *args, **kwargs):
         # разрешение для просмотра
+        #permission_required = False
         return super(StepDetailViewSet, self).retrieve(request, *args, **kwargs)
+
+    @permission_required(perm='lessons.can_change_step')
+    def partial_update(self, request, *args, **kwargs):
+        #permission_required = 'lessons.can_change_step'
+        return super(StepDetailViewSet, self).partial_update(request, *args, **kwargs)
+
+    """
+    @permission_required('lessons.can_change_step')
+    def update(self, request, *args, **kwargs):
+        return super(StepDetailViewSet, self).update(request, *args, **kwargs)"""
+
+
+
 
 
 
