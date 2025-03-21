3e25a7850a5bc177720857118696abc74c8ebf7e
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-03-21 16:46:41 +0300

    Step CRUD + права + тесты

--- backend/lessons/urls.py
+++ backend/lessons/urls.py
@@ -14,11 +14,11 @@ router.register(prefix=r'events', viewset=views.EventViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     # url: step/ только list и create
-    path('', views.StepViewSet.as_view({'get':'list',
+    path('step/', views.StepViewSet.as_view({'get':'list',
                                   'post':'create'}),
                                     name='step'),
     # url: step/[0-9]+ retrieve partial_update destroy
-    path('<int:pk>/', views.StepDetailViewSet.as_view({'get':'retrieve',
+    path('step/<int:pk>/', views.StepDetailViewSet.as_view({'get':'retrieve',
                                                'patch':'partial_update',
                                                'delete':'destroy'
                                                }),name='step_detail'),
