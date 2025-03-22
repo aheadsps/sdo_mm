de5e3d1a57db0021bfea740a0b309a7f1299635a
Vera869 <verea8910@gmail.com>
2025-03-22 17:35:45 +0700

    + loader

--- frontend/src/pages/my-learning/MyLearning.tsx
+++ frontend/src/pages/my-learning/MyLearning.tsx
@@ -9,11 +9,13 @@ import { courses, lessons } from './mockData'
 import s from './myLearning.module.scss'
 import { Course, Lesson } from './types'
 import { AIComponent } from '@shared/components/ai-component/AIComponent'
+import Loader from '@shared/components/loader/Loader'
 
 const MyLearningComp: React.FC = () => {
   const [mode, setMode] = useState<string>('Назначенные курсы')
   const [isTooltipe, setIsTooltipe] = useState<boolean>(true)
-  const [isAIOpen, setIsAIOpen] = useState<boolean>(true)
+  const [isAIOpen, setIsAIOpen] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   const buttons = [
     'Назначенные курсы',
@@ -21,47 +23,56 @@ const MyLearningComp: React.FC = () => {
     'Избранные курсы',
     'Завершённые курсы',
   ]
-
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 3000)
+  
   return (
     <>
       {isTooltipe && <Tooltipe />}
       {isAIOpen && <AIComponent />}
       <main className={s.container}>
-        <div className={s.container__headBox}>
-          <div className={s.container__btnBox}>
-            {buttons.map((btn, index) => {
-              const count = index + 1
-              return (
-                <div className={s.container__boxbtn} key={index}>
-                  <div className={s.container__counterBox}>
-                    <p className={s.container__counter}>{count}</p>
-                  </div>
-                  <Button
-                    className={s.container__btn}
-                    children={btn}
-                    variant={mode === btn ? 'primary' : 'secondary'}
-                    onClick={() => setMode(btn)}
-                  />
-                </div>
-              )
-            })}
-          </div>
-          <Button
-            children="ИИ"
-            variant={mode === 'ИИ' ? 'primary' : 'secondary'}
-            onClick={() => setMode('ИИ')}
-          />
-        </div>
-        <div className={s.container__content}>
-          {courses.map((course: Course) => {
-            return <LessonCard course={course} key={course.id} />
-          })}
-        </div>
-        <div className={s.container__content}>
-          {lessons.map((lesson: Lesson) => {
-            return <LessonCard course={lesson} key={lesson.id} />
-          })}
-        </div>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <div className={s.container__headBox}>
+              <div className={s.container__btnBox}>
+                {buttons.map((btn, index) => {
+                  const count = index + 1
+                  return (
+                    <div className={s.container__boxbtn} key={index}>
+                      <div className={s.container__counterBox}>
+                        <p className={s.container__counter}>{count}</p>
+                      </div>
+                      <Button
+                        className={s.container__btn}
+                        children={btn}
+                        variant={mode === btn ? 'primary' : 'secondary'}
+                        onClick={() => setMode(btn)}
+                      />
+                    </div>
+                  )
+                })}
+              </div>
+              <Button
+                children="ИИ"
+                variant={mode === 'ИИ' ? 'primary' : 'secondary'}
+                onClick={() => setMode('ИИ')}
+              />
+            </div>
+            <div className={s.container__content}>
+              {courses.map((course: Course) => {
+                return <LessonCard course={course} key={course.id} />
+              })}
+            </div>
+            <div className={s.container__content}>
+              {lessons.map((lesson: Lesson) => {
+                return <LessonCard course={lesson} key={lesson.id} />
+              })}
+            </div>
+          </>
+        )}
       </main>
     </>
   )
