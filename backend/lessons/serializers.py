15a7593a7c513e9f1d64828a252952a6bb159bcf
mexic <mexic2284@mail.ru>
2025-03-23 20:43:59 +0300

    rebase

--- backend/lessons/serializers.py
+++ backend/lessons/serializers.py
@@ -148,67 +148,6 @@ class EventSerializer(serializers.ModelSerializer):
         )
 
 
-class LessonCreateSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор создания и обновления урока
-    """
-    # step = StepSerializer(many=True)
-    # "step", "test_block добавить
-    class Meta:
-        model = models.Lesson
-        fields = (
-            "id",
-            "name",
-            "serial",
-            "course",
-        )
-        read_only_fields = ("id",)
-
-    # def create(self, validated_data):
-    #     step_items = validated_data.pop('step', [])
-    #     lesson = Lesson.objects.create(**validated_data)
-    #     step_create = [
-    #         Step(**step, lesson=lesson) for step in step_items
-    #     ]
-    #     Step.objects.bulk_create(step_create)
-    #     return lesson
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор оптимизированного вывода
-    """
-    # "step", "test_block добавить
-
-    class Meta:
-        model = models.Lesson
-        fields = (
-            "id",
-            "name",
-            "serial",
-            "course",
-        )
-
-
-class LessonViewSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор детального представления урока
-    """
-    course = ViewCourseSerializer()
-    # step = StepViewSerializer(many=True, read_only=True)
-    # test_block = TestBlockViewSerializer(read_only=True)
-
-    # "step", "test_block добавить
-    class Meta:
-        model = models.Lesson
-        fields = (
-            "id",
-            "name",
-            "serial",
-            "course",
-        )
-
-
 class EventSerializerCreate(serializers.ModelSerializer):
     """
     Сериализатор Создания в Изменения Эвента
@@ -275,3 +214,64 @@ class EventSerializerCreate(serializers.ModelSerializer):
         self._correct_status(self.validated_data)
         logger.debug(f'after {self.validated_data}')
         return super().save(**kwargs)
+
+
+class LessonCreateSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор создания и обновления урока
+    """
+    # step = StepSerializer(many=True)
+    # "step", "test_block добавить
+    class Meta:
+        model = models.Lesson
+        fields = (
+            "id",
+            "name",
+            "serial",
+            "course",
+        )
+        read_only_fields = ("id",)
+
+    # def create(self, validated_data):
+    #     step_items = validated_data.pop('step', [])
+    #     lesson = Lesson.objects.create(**validated_data)
+    #     step_create = [
+    #         Step(**step, lesson=lesson) for step in step_items
+    #     ]
+    #     Step.objects.bulk_create(step_create)
+    #     return lesson
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор оптимизированного вывода
+    """
+    # "step", "test_block добавить
+
+    class Meta:
+        model = models.Lesson
+        fields = (
+            "id",
+            "name",
+            "serial",
+            "course",
+        )
+
+
+class LessonViewSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор детального представления урока
+    """
+    course = ViewCourseSerializer()
+    # step = StepViewSerializer(many=True, read_only=True)
+    # test_block = TestBlockViewSerializer(read_only=True)
+
+    # "step", "test_block добавить
+    class Meta:
+        model = models.Lesson
+        fields = (
+            "id",
+            "name",
+            "serial",
+            "course",
+        )
