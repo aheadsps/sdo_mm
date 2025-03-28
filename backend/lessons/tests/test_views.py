cda0ba676981dd62a33715ffeb21089367c815f4
Alex Pavlov <yukiu217@gmail.com>
2025-03-29 01:16:14 +0600

    fix2

--- backend/lessons/tests/test_views.py
+++ backend/lessons/tests/test_views.py
@@ -1,7 +1,9 @@
+import json
 import datetime
 
 from lessons.models import Course, Lesson, TestBlock
 from rest_framework import status
+from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 
