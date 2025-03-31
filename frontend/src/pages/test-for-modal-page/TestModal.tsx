1bedbeaeb75623e30c67b3e10f4b90e1cdbb9ba3
tanya-kostecki <tanya.pavlova1294@gmail.com>
2025-03-31 18:24:49 +0200

    reusable progress.bar with example

--- frontend/src/pages/test-for-modal-page/TestModal.tsx
+++ frontend/src/pages/test-for-modal-page/TestModal.tsx
@@ -1,12 +1,20 @@
-import { Button, Modal } from '@shared/components'
+import { Button, Modal, ProgressBar } from '@shared/components'
 import { withLayout } from '@shared/HOC'
 import { useToggle } from '@shared/hooks/useToggle'
+
+import s from './example.module.scss'
 
 const Test = () => {
   const { isOpen: isOpenModal, close: closeModal, open: openModal } = useToggle()
   return (
     <>
       <Button onClick={openModal}>Open Modal</Button>
+      <ProgressBar
+        progress={2}
+        total={4}
+        progressBarClassName={s.progressBar}
+        progressIndicatorClassName={s.progressIndicator}
+      />
       {isOpenModal && (
         <Modal close={closeModal} title="Modal Window">
           Hello Modal
