933d2ada4a18a51289312a7d9c36574f1bd0c5c5
Alex Pavlov <yukiu217@gmail.com>
2025-04-02 12:27:26 +0600

    fill abstract

--- backend/lessons/scorm/engine/core/abc.py
+++ backend/lessons/scorm/engine/core/abc.py
@@ -6,5 +6,21 @@ class AbstractCore(ABC):
     Астрактрый класс для назначение протокола
     """
 
+    @property
+    @abstractmethod
+    def manifest(self): ...
+
+    @property
+    @abstractmethod
+    def meta(self): ...
+
+    @property
+    @abstractmethod
+    def organizations(self): ...
+
+    @property
+    @abstractmethod
+    def resources(self): ...
+
     @abstractmethod
     def save(self): ...
