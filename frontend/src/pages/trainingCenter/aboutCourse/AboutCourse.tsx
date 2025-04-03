57597e3f640ac5da0ee461a22d75a8420f8202ae
Vera869 <verea8910@gmail.com>
2025-04-03 16:02:19 +0700

    +CalendarSelect

--- frontend/src/pages/trainingCenter/aboutCourse/AboutCourse.tsx
+++ frontend/src/pages/trainingCenter/aboutCourse/AboutCourse.tsx
@@ -1,4 +1,5 @@
 import { Select } from '@shared/components'
+import { CalendarSelect } from '@shared/components/calendarSelect/CalendarSelect'
 
 import s from './aboutCourse.module.scss'
 type Option = {
@@ -18,19 +19,26 @@ const teachers: Option[] = [
   { id: 13, value: 'Петров П.П.' },
   { id: 14, value: 'Гуру А.С.' },
 ]
+
+const dates: Option[] = [
+  { id: 101, value: '25.06.2024' },
+  { id: 102, value: '13.02.2025' },
+  { id: 103, value: '04.07.2022' },
+  { id: 104, value: '18.12.2024' },
+]
 export const AboutCourse = () => {
   return (
     <div className={s.container}>
       <div className={s.leftBlock}>
         <h3 className={s.leftBlock__title}>Основная информация</h3>
-        <Select options={courseStatuses} placeholder="Дата создания" />
-        <Select options={courseStatuses} placeholder="Дата окончания" />
+        <CalendarSelect options={dates} placeholder="Дата создания" className={s.select} />
+        <CalendarSelect options={dates} placeholder="Дата окончания" className={s.select} />
         <h6 className={s.leftBlock__subtitle}>
           <p className={s.sutitleLeft}>Количество студентов:</p>
           <p className={s.sutitleRight}>654</p>
         </h6>
-        <Select options={courseStatuses} placeholder="Статус курса" />
-        <Select options={teachers} placeholder="Преподаватель" />
+        <Select options={courseStatuses} placeholder="Статус курса" className={s.select} />
+        <Select options={teachers} placeholder="Преподаватель" className={s.select} />
       </div>
       <div className={s.rightBlock}></div>
     </div>
