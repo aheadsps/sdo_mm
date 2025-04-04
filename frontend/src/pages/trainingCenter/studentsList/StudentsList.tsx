da5d7182738fb41def295486978a66479b2c3022
Vera869 <verea8910@gmail.com>
2025-04-04 17:38:49 +0700

    +search

--- frontend/src/pages/trainingCenter/studentsList/StudentsList.tsx
+++ frontend/src/pages/trainingCenter/studentsList/StudentsList.tsx
@@ -1,4 +1,5 @@
 import { Search } from '@shared/components/search'
+import { StudentBlock } from '@shared/components/studentBlock'
 
 import s from './studentsList.module.scss'
 
@@ -6,7 +7,12 @@ export const StudentList = () => {
   return (
     <div className={s.container}>
       <Search />
-      <div className={s.content}></div>
+      <div className={s.content}>
+        <StudentBlock />
+        <StudentBlock />
+        <StudentBlock />
+        <StudentBlock />
+      </div>
     </div>
   )
 }
---
+++ frontend/src/shared/components/studentBlock/StudentBlock.tsx
@@ -0,0 +1,5 @@
+import s from './studentBlock.module.scss'
+
+export const StudentBlock = () => {
+  return <div className={s.box}>{/* <div className={s.content}></div> */}</div>
+}
