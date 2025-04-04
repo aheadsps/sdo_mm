64d3071eea9692ea5b2273c0d67262e347858be1
Vera869 <verea8910@gmail.com>
2025-04-04 18:27:51 +0700

    +BlockHeader

--- frontend/src/pages/trainingCenter/studentsList/StudentsList.tsx
+++ frontend/src/pages/trainingCenter/studentsList/StudentsList.tsx
@@ -1,13 +1,18 @@
 import { Search } from '@shared/components/search'
 import { StudentBlock } from '@shared/components/studentBlock'
 
+import { BlockHeader } from '../course/program/block-header/BlockHeader'
+
 import s from './studentsList.module.scss'
+
+const columns: string[] = ['Студент', 'Прогресс', 'Действия']
 
 export const StudentList = () => {
   return (
     <div className={s.container}>
       <Search />
       <div className={s.content}>
+        <BlockHeader columns={columns} />
         <StudentBlock />
         <StudentBlock />
         <StudentBlock />
