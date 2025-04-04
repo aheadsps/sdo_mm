5e1c7ab1822a14fe518aada239728e85d77ec7f1
Vera869 <verea8910@gmail.com>
2025-04-04 19:28:39 +0700

    + ProgressBar Styled

--- frontend/src/pages/trainingCenter/studentsList/studentBlock/StudentBlock.tsx
+++ frontend/src/pages/trainingCenter/studentsList/studentBlock/StudentBlock.tsx
@@ -1,20 +1,29 @@
 import { Button, ProgressBar } from '@shared/components'
 
+import { BasketIcon, WebinarAndSupportIcon } from '@assets/icons'
+
 import s from './studentBlock.module.scss'
-import { BasketIcon } from '@assets/icons'
 
 export const StudentBlock = () => {
   return (
     <div className={s.box}>
       <div className={s.name}>Кузнецов Евгений Андреевич</div>
       <div className={s.progress}>
-        <ProgressBar progress={40} total={100} />
+        <p className={s.progress__txt}>20 из 100%</p>
+        <ProgressBar
+          progress={40}
+          total={100}
+          progressBarClassName={s.bar}
+          progressIndicatorClassName={s.indicator}
+        />
       </div>
       <div className={s.btns}>
         <Button variant="secondary" className={s.btn}>
           <BasketIcon width={'40px'} height={'40px'} />
         </Button>
-        <Button variant="primary" className={s.btn_prim} />
+        <Button variant="primary" className={s.btn}>
+          <WebinarAndSupportIcon width={'24px'} height={'24px'} />
+        </Button>
       </div>
     </div>
   )
