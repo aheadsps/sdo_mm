8d28afebb7c8d2516caa7ba95807c7b25c0fd555
Vera869 <verea8910@gmail.com>
2025-04-06 11:25:14 +0700

    + corrected search to merge develop

--- frontend/src/shared/components/search/Search.tsx
+++ frontend/src/shared/components/search/Search.tsx
@@ -1,4 +1,49 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+
+// import { SearchIcon } from '@assets/icons'
+// import { studentType } from '@pages/trainingCenter/studentsList/data'
+
+// import { Input } from '../text-field'
+
+// import s from './Search.module.scss'
+// type Props = {
+//   students?: studentType[]
+//   className?: string
+// }
+// export const Search = ({ students }: Props) => {
+//   const [isDatalistVisible, setDatalistVisible] = useState<boolean>(false)
+//   return (
+//     <div className={s.container}>
+//       <div className={s.box}>
+//         <Input
+// list="variants"
+// id="search"
+// className={s.input}
+// placeholder="Поиск"
+// type="text"
+// onFocus={() => setDatalistVisible(true)}
+//         />
+// {isDatalistVisible && students ? (
+//   <datalist id="search" className={s.list}>
+//     {students.map((student) => {
+//       return (
+//         <option value="student.id" key={student.id} className={s.option}>
+//           {student.name}
+//         </option>
+//       )
+//     })}
+//   </datalist>
+// ) : (
+//   ''
+// )}
+//         <SearchIcon className={s.icon} onClick={() => setDatalistVisible(false)} />
+//       </div>
+//     </div>
+//   )
+// }
+
+import clsx from 'clsx'
+import { ComponentPropsWithRef, useState } from 'react'
 
 import { SearchIcon } from '@assets/icons'
 import { studentType } from '@pages/trainingCenter/studentsList/data'
@@ -6,22 +51,26 @@ import { studentType } from '@pages/trainingCenter/studentsList/data'
 import { Input } from '../text-field'
 
 import s from './Search.module.scss'
+
 type Props = {
-  students?: studentType[]
   className?: string
-}
-export const Search = ({ students }: Props) => {
+  students?: studentType[]
+} & ComponentPropsWithRef<'input'>
+export const Search = ({ className, students, onBlur, onFocus, autoFocus }: Props) => {
   const [isDatalistVisible, setDatalistVisible] = useState<boolean>(false)
   return (
-    <div className={s.container}>
+    <div className={clsx(s.container, className)}>
       <div className={s.box}>
         <Input
-          list="variants"
-          id="search"
           className={s.input}
           placeholder="Поиск"
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onChange={() => setDatalistVisible(true)}
+          autoFocus={autoFocus}
+          list="variants"
+          id="search"
           type="text"
-          onFocus={() => setDatalistVisible(true)}
         />
         {isDatalistVisible && students ? (
           <datalist id="search" className={s.list}>
