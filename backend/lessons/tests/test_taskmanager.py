0ae932de7c1bb87aa3ee1e0a3ab3f948b8ad680a
Serg Kuchukov Office <kuchukov.sergey@gmail.ru>
2025-04-07 12:52:50 +0300

    fixed taskmanager  методы delete

--- backend/lessons/tests/test_taskmanager.py
+++ backend/lessons/tests/test_taskmanager.py
@@ -2,9 +2,7 @@ from rest_framework.test import APITestCase, APIClient
 from users import models as users_models
 from django.contrib.auth import get_user_model
 import datetime
-from pathlib import Path
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
 from users.models import WorkExperience
 from lessons.models import Course
 from lessons.taskmanager import TaskManager
@@ -56,16 +54,15 @@ class TestStepUrl(APITestCase):
         date_time_str = '2038-06-29 08:15:27.243860'
         date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
         # создается задача
-        TaskManager(1, self.user.pk, [1], date_time_obj).create()
-        TaskManager(1, self.user.pk, [1], date_time_obj).create()
+        TaskManager(1, [1], date_time_obj).create()
+        TaskManager(1, [1], date_time_obj).create()
 
         # проверка
         rezults = PeriodicTask.objects.all()
         for rezult in rezults:
             rezult_ok = {
                 "course_id": 1,
-                "user_creata": self.user.pk,
-                "user": [1],
+                "users": [1],
                 "start_date": "2038-06-29 08:15",
                 "end_date": None
             }
@@ -75,39 +72,62 @@ class TestStepUrl(APITestCase):
         """
         Изменение задачи для эвента
         """
-        id_task_1 = 1
-        TaskManager(1, self.user.pk, [2], date_time_obj).upload(id_task_1)
+
+        TaskManager(1, [1, 2], date_time_obj).upload()
 
         date_time_str2 = '2038-06-30 09:00:00.000000'
         date_time_obj2 = datetime.datetime.strptime(date_time_str2, '%Y-%m-%d %H:%M:%S.%f')
 
+        TaskManager(1, [1], date_time_obj2).create()
+        TaskManager(1, [2], date_time_obj2).create()
         """
         Изменение задачи для эвента и шедулера
         """
-        id_task_2 = 2
-        TaskManager(1, self.user.pk, [1, 2], date_time_obj2).upload(id_task_2)
+
+        TaskManager(1, [2], date_time_obj).upload()
 
         # проверка
         rezults_2 = PeriodicTask.objects.all()
 
         rezults_json_1 = json.dumps({"course_id": 1,
-                                     "user_creata": 53,
-                                     "user": [2],
+                                     "users": [2],
                                      "start_date": "2038-06-29 08:15",
                                      "end_date": None,
                                      })
-        self.assertEqual(rezults_2[0].kwargs, rezults_json_1)
+        self.assertEqual(rezults_2[1].kwargs, rezults_json_1)
 
         rezults_json_2 = json.dumps({"course_id": 1,
-                                     "user_creata": 53,
-                                     "user": [1, 2],
+                                     "users": [1, 2],
+                                     "start_date": "2038-06-30 09:00",
+                                     "end_date": None,
+                                     })
+        self.assertEqual(rezults_2[0].kwargs, rezults_json_2)
+
+        self.assertEqual(rezults_2[1].schedule.clocked_time.strftime("%Y-%m-%d %H:%M"),
+                         "2038-06-29 05:15")
+
+        self.assertEqual(rezults_2[0].schedule.clocked_time.strftime("%Y-%m-%d %H:%M"),
+                         "2038-06-30 06:00")
+
+
+        """
+        А если нет такой задачи? 
+        """
+        with self.assertRaises(ObjectDoesNotExist):
+            TaskManager(2, [2], date_time_obj).upload()
+
+        """
+        Удаляем пользователя [1] из списка
+        """
+        TaskManager(1, [1], date_time_obj2).delete()
+        rezults_2 = PeriodicTask.objects.all()
+        rezults_json_2 = json.dumps({"course_id": 1,
+                                     "users": [2],
                                      "start_date": "2038-06-30 09:00",
                                      "end_date": None,
                                      })
         self.assertEqual(rezults_2[1].kwargs, rezults_json_2)
 
-        self.assertEqual(rezults_2[0].schedule.clocked_time.strftime("%Y-%m-%d %H:%M"),
-                         "2038-06-29 05:15")
 
-        self.assertEqual(rezults_2[1].schedule.clocked_time.strftime("%Y-%m-%d %H:%M"),
-                         "2038-06-30 06:00")
+
+
