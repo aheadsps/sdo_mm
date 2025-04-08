00e27491134a8377080846df3f52620eb8806cd4
Vera869 <verea8910@gmail.com>
2025-04-08 18:42:53 +0700

    + tooltipe

--- frontend/src/pages/constructor/ConstructorPage.tsx
+++ frontend/src/pages/constructor/ConstructorPage.tsx
@@ -1,13 +1,23 @@
 import { CMenu, Title } from '@shared/components'
+// import { Tooltipe } from '@shared/components/tooltipe'
 import { withLayout } from '@shared/HOC'
+// import { useToggle } from '@shared/hooks'
 
 import SettingsIcon from '@assets/icons/SettingsIcon'
 
 import s from './constructorPage.module.scss'
 
 const Constructor: React.FC = () => {
+  // const { isOpen: isTooltipeOpen, close: closeTooltipe } = useToggle(true)
   return (
     <div className={s.wrapper}>
+      {/* {isTooltipeOpen && (
+        <Tooltipe
+          txt="Вы можете добавлять блоки в любом порядке, создавая курс под свои нужды"
+          close={closeTooltipe}
+          className="s.span"
+        />
+      )} */}
       <Title
         txt="Конструктор курса"
         btn0={<SettingsIcon />}
