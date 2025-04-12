29049b0a269b6e7370a53413d794ded6da25c2ff
mexic <mexic2284@mail.ru>
2025-04-13 00:29:34 +0300

    добавлена глобальная переменна TYPE_OF_ASSESSMENT для моодели AssessmentSubmission к полю type_of

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -325,6 +325,16 @@ TYPE_LESSON = [
         "линейный",
     )
 ]
+TYPE_OF_ASSESSMENT = [
+    (
+        "answer",
+        "ответ"
+    ),
+    (
+        "question",
+        "вопрос"
+    )
+]
 
 
 
