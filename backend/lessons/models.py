2ec9b3b0ce631ff39f1d0206e63f72ec65158bce
mexic <mexic2284@mail.ru>
2025-04-13 16:50:16 +0300

    в модель TestBlock добавлены поля end_date, status, max_score

--- backend/lessons/models.py
+++ backend/lessons/models.py
@@ -613,11 +613,11 @@ class CourseProgress(models.Model):
                                    related_name="course_progress",
                                    help_text="Тест блок"
                                    )
-    score = models.DecimalField(_("Оценка"),
+    score = models.DecimalField(_("Балл"),
                                 max_digits=5,
                                 decimal_places=2,
                                 default=0.00,
-                                help_text="Оценка"
+                                help_text="Балл"
                                 )
     data_assessment = models.DecimalField(_("Успеваемость на момент даты"),
                                           auto_now_add=True,
