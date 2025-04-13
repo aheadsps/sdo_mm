c787d5a8d2651e26600b1087db4d95dbf5851a08
mexic <mexic2284@mail.ru>
2025-04-13 16:50:16 +0300

    в модель TestBlock добавлены поля end_date, status, max_score

--- backend/lessons/models.py
+++ backend/lessons/models.py
@@ -278,13 +278,27 @@ class TestBlock(models.Model):
     """
     Модель тестового блока
     """
-
+    end_date = models.DateTimeField(_("Дата"),
+                                    auto_now_add=True,
+                                    help_text="Дата"
+                                    )
+    status = models.CharField(_("Статус"),
+                              max_length=10,
+                              choices=settings.TYPE_TEST_BLOCK,
+                              help_text="Статус"
+                              )
     lesson = models.OneToOneField(Lesson,
                                   on_delete=models.CASCADE,
                                   related_name="test_block",
                                   null=True,
                                   blank=True,
                                   )
+    max_score = models.DateTimeField(_("Максимальный балл"),
+                                     max_digits=5,
+                                     decimal_places=2,
+                                     default=0.00,
+                                     help_text="Максимальный балл"
+                                     )
 
     class Meta:
         verbose_name = "тестовый блок"
@@ -520,11 +534,11 @@ class CourseProgress(models.Model):
                                    related_name="course_progress",
                                    help_text="Тест блок"
                                    )
-    score = models.DecimalField(_("Оценка"),
+    score = models.DecimalField(_("Балл"),
                                 max_digits=5,
                                 decimal_places=2,
                                 default=0.00,
-                                help_text="Оценка"
+                                help_text="Балл"
                                 )
     data_assessment = models.DecimalField(_("Успеваемость на момент даты"),
                                           auto_now_add=True,
