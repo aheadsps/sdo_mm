103c1eb7d347f3e6b650600b279c5434f47f4424
Serg PC <kuchukov.sergey@gmail.com>
2025-04-13 18:38:06 +0300

    установка селери

---
+++ backend/config/celery.py
@@ -0,0 +1,10 @@
+from config.settings import CELERY_BROKER_URL
+from celery import Celery
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery(__name__, broker=CELERY_BROKER_URL)
+app.config_from_object('config.settings', namespace='CELERY')
+app.autodiscover_tasks(['lessons'])
+app.conf.timezone = "Europe/Moscow"
