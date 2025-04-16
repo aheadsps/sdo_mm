8f87877f64630c634c62b1b27f3287cd331ec145
mexic <mexic2284@mail.ru>
2025-04-17 01:57:31 +0300

    в сериализатор добавлены AssessmentSubmissionSerializer валидаторы: - TaskEssayQuestionValidator - проверяет что в тест блоке не более 1 вопроса типа task или essay. - NoAnswerForTaskEssayValidator - проверят для вопросов типа task или essay не прикриплялся ответ

--- backend/lessons/serializers.py
+++ backend/lessons/serializers.py
@@ -195,6 +195,10 @@ class AssessmentSubmissionSerializer(serializers.ModelSerializer):
                   "files",
                   "date_assessment",
                   )
+        validators = (
+            validators.TaskEssayQuestionValidator(),
+            validators.NoAnswerForTaskEssayValidator('answer'),
+        )
 
 
 class StepSerializer(serializers.ModelSerializer):
