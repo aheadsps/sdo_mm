330392cfa06d4bb5330b35cf8c91e42ce2a6d33e
tanya-kostecki <tanya.pavlova1294@gmail.com>
2025-04-16 18:27:43 +0200

    minor style fix in icons

--- frontend/src/assets/icons/PaintIcon.tsx
+++ frontend/src/assets/icons/PaintIcon.tsx
@@ -7,6 +7,7 @@ const PaintIcon = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
       height="10"
       viewBox="0 0 12 10"
       xmlns="http://www.w3.org/2000/svg"
+      fill="none"
       {...props}
       ref={ref}
     >
