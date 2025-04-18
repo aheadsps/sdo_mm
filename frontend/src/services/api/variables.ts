96fe1ac714a68152c9cc32cc2c89d7a906835e26
pktolstov <80717154+pktolstov@users.noreply.github.com>
2025-04-18 10:49:34 +0300

    Update variables.ts

--- frontend/src/services/api/variables.ts
+++ frontend/src/services/api/variables.ts
@@ -1,5 +1,5 @@
-//export const baseUrl = 'http://localhost:8080/api/v1'
-export const baseUrl = `https://${location.hostname}/api/v1`
+export const baseUrl = 'http://localhost:8080/api/v1'
+//export const baseUrl = `https://${location.hostname}/api/v1`
 
 export const getToken = () => {
   const token = localStorage.getItem('token')
