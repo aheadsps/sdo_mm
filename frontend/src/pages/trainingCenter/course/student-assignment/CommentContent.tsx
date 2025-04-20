b4005cb8534c517d0fd29a2461c25514e2d691a7
tanya-kostecki <tanya.pavlova1294@gmail.com>
2025-04-20 23:15:51 +0200

    finish layout for the page

--- frontend/src/pages/trainingCenter/course/student-assignment/CommentContent.tsx
+++ frontend/src/pages/trainingCenter/course/student-assignment/CommentContent.tsx
@@ -1,3 +1,29 @@
-export const CommentContent = () => {
-  return <div>Hello content</div>
+import { Typography } from '@shared/components'
+
+import s from './student-assignment.module.scss'
+
+type Props = {
+  isInternalComment?: boolean
+  children: string
+}
+export const CommentContent = ({ isInternalComment, children }: Props) => {
+  return (
+    <div>
+      <div className={s.commentOwner}>
+        <Typography variant="body_1">Мария Комаровa</Typography>
+        {isInternalComment && (
+          <Typography variant="body_1" className={s.internal}>
+            Внутренний
+          </Typography>
+        )}
+      </div>
+      <div className={s.dateTime}>
+        <Typography variant="caption">04.05.25</Typography>
+        <Typography variant="caption">17:30</Typography>
+      </div>
+      <Typography className={s.commentText} variant="body_2">
+        {children}
+      </Typography>
+    </div>
+  )
 }
