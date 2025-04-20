90d72369d0dd73495b650fb76929192c392d33f7
tanya-kostecki <tanya.pavlova1294@gmail.com>
2025-04-20 21:05:53 +0200

    block the main page for methodologist

--- frontend/src/features/main/ui/Main.tsx
+++ frontend/src/features/main/ui/Main.tsx
@@ -1,7 +1,10 @@
+import { routes } from '@routes/routes'
 import { selectUser } from '@services/slices'
 import { useAppSelector } from '@services/store'
 import { Schedule, Typography } from '@shared/components'
 import { Role } from '@shared/components/sidebar/sidebar.types'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import s from './main.module.scss'
 import { CurrentTasks } from './tasks-elements/CurrentTasks'
@@ -11,7 +14,14 @@ import { WarningCard } from './warning-card'
 export const Main = () => {
   const user = useAppSelector(selectUser)
   const isStudent = user?.profession === Role.student
-  console.log(isStudent)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isStudent) {
+      navigate(routes.trainingCenter, { replace: true })
+    }
+  }, [isStudent, navigate])
+
   return (
     <>
       {isStudent ? (
