2a3bc52793f05d73a65db5ec93797c52cf386db5
Vera869 <verea8910@gmail.com>
2025-04-20 12:49:33 +0700

    + fix types files conflicts

---
+++ frontend/src/services/api/types.api.ts
@@ -0,0 +1,28 @@
+// import { NewItem } from '@services/slices/constructor/constructor.types'
+// export type Step = {
+//   attachments: Attachments[]
+//   content_text: string
+//   id: number
+//   lesson: number
+//   serial: number
+//   teacher: number
+//   title: string
+// } & {
+//   blockItems: NewItem[]
+// }
+
+// export type LessonType = {
+//   course: number
+//   id: number
+//   name: string
+//   resource: string | null //????
+//   serial: number
+//   start_date: string
+//   started: boolean
+//   steps: Step[]
+//   teacher: number
+//   test_block: number
+//   version: string | null //???
+// } & {
+//   expanded: boolean
+// }
