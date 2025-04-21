a1ef2eaff00d095816629281b8520dfaa0395e6e
Elizaveta <you@example.com>
2025-04-21 19:45:41 +0300

    add tests course

--- backend/lessons/tests/test_course.py
+++ backend/lessons/tests/test_course.py
@@ -1,145 +1,168 @@
-
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from datetime import datetime
 
-from lessons.models import Course
+from lessons.models import Course, SCORM
 from users.models import Profession, WorkExperience
-from lessons.models import SCORM
 
 
 class CourseModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Создаем тестовые данные
-        cls.profession = Profession.objects.create(name="Тестовая профессия")
-        cls.scorm = SCORM.objects.create(title="Тестовый SCORM")
-        cls.experience = WorkExperience.objects.create(years=1)
+        cls.profession = Profession.objects.create(verbose_name="Name Profession")
+        cls.experience = WorkExperience.objects.create(years=2)
+        cls.scorm = SCORM.objects.create(title="Test SCORM")
 
-        # Создаем тестовое изображение
-        cls.test_image = SimpleUploadedFile(
-            name='test_image.jpg',
-            content=b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00',
-            content_type='image/jpeg'
-        )
-
-        # Основной тестовый курс
         cls.course = Course.objects.create(
-            name="Тестовый курс",
-            description="Тестовое описание",
-            image=cls.test_image,
+            verbose_name="Name Profession",
+            description="Introduction",
+            beginer=True,
             profession=cls.profession,
             scorm=cls.scorm
         )
         cls.course.experiences.add(cls.experience)
 
-    def test_model_fields(self):
-        """Проверка корректности заполнения полей модели"""
-        course = self.course
+    def test_field_labels(self):
+        """Проверка verbose_name полей"""
+        self.assertEqual(Course._meta.get_field('name').verbose_name, _("Название"))
+        self.assertEqual(Course._meta.get_field('description').verbose_name, _("Описание"))
+        self.assertEqual(Course._meta.get_field('beginer').verbose_name, _("Начинающий"))
 
-        self.assertEqual(course.name, "Тестовый курс")
-        self.assertEqual(course.description, "Тестовое описание")
-        self.assertTrue(course.image.name.startswith('courses/'))
-        self.assertEqual(course.profession, self.profession)
-        self.assertEqual(course.scorm, self.scorm)
-        self.assertIn(self.experience, course.experiences.all())
+    def test_field_help_texts(self):
+        """Проверка help_text полей"""
+        self.assertEqual(Course._meta.get_field('name').help_text, "Название курса")
+        self.assertEqual(Course._meta.get_field('description').help_text, "Описание курса")
+        self.assertEqual(Course._meta.get_field('beginer').help_text, "Курс для начинающих")
 
-    def test_auto_datetime_fields(self):
-        """Проверка автоматических полей даты/времени"""
-        now = timezone.now()
+    def test_auto_dates(self):
+        """Проверка автоматического заполнения дат"""
+        self.assertIsNotNone(self.course.create_date)
+        self.assertIsNotNone(self.course.update_date)
 
-        self.assertLessEqual(self.course.create_date, now)
-        self.assertLessEqual(self.course.update_date, now)
-
-    def test_beginer_default_value(self):
-        """Проверка значения по умолчанию для beginer"""
-        new_course = Course.objects.create(name="Новый курс")
-        self.assertFalse(new_course.beginer)
-
-    def test_update_date_auto_update(self):
-        """Проверка автообновления update_date"""
-        original_date = self.course.update_date
-        self.course.name = "Обновленное имя"
+        # Проверка обновления update_date
+        old_update = self.course.update_date
+        self.course.name = "Updated Name"
         self.course.save()
-        self.assertGreater(self.course.update_date, original_date)
+        self.assertGreater(self.course.update_date, old_update)
 
-    def test_foreign_key_relations(self):
-        """Проверка отношений ForeignKey"""
-        # Проверка SET_NULL для profession
-        self.profession.delete()
-        self.course.refresh_from_db()
-        self.assertIsNone(self.course.profession)
-
-        # Проверка SET_NULL для scorm
-        self.scorm.delete()
-        self.course.refresh_from_db()
-        self.assertIsNone(self.course.scorm)
-
-    def test_str_representation(self):
-        """Проверка строкового представления"""
-        self.assertEqual(str(self.course), "Тестовый курс")
-
-    def test_verbose_names(self):
-        """Проверка verbose names"""
-        self.assertEqual(Course._meta.verbose_name, "Course")
-        self.assertEqual(Course._meta.verbose_name_plural, "Courses")
-
-    def test_field_verbose_names(self):
-        """Проверка verbose names отдельных полей"""
-        field_verboses = {
-            'name': 'Название',
-            'description': 'Описание',
-            'beginer': 'Начинающий',
-            'create_date': 'Дата создания',
-            'update_date': 'Дата обновления',
-            'image': 'Превью',
-            'profession': 'профессия',
-            'scorm': 'SCORM',
-            'experiences': 'Стаж',
-        }
-
-        for field, expected_value in field_verboses.items():
-            with self.subTest(field=field):
-                verbose_name = self.course._meta.get_field(field).verbose_name
-                self.assertEqual(verbose_name, expected_value)
-
-    def test_help_texts(self):
-        """Проверка help texts"""
-        help_texts = {
-            'name': 'Название курса',
-            'description': 'Описание курса',
-            'beginer': 'Курс для начинающих',
-            'create_date': 'Дата создания курса',
-            'update_date': 'Дата обновления курса',
-            'experiences': 'На какие стажи расчитан курс',
-        }
-
-        for field, expected_value in help_texts.items():
-            with self.subTest(field=field):
-                help_text = self.course._meta.get_field(field).help_text
-                self.assertEqual(help_text, expected_value)
-
-    def test_ordering(self):
-        """Проверка сортировки по умолчанию (если есть)"""
-        # Если в Meta есть ordering, добавить соответствующие проверки
-        pass
-
-    def test_m2m_relation(self):
-        """Проверка отношения ManyToMany"""
-        new_experience = WorkExperience.objects.create(years=3)
-        self.course.experiences.add(new_experience)
-        self.assertEqual(self.course.experiences.count(), 2)
-
-        self.course.experiences.remove(new_experience)
+    def test_relations(self):
+        """Проверка связей модели"""
+        self.assertEqual(self.course.profession, self.profession)
+        self.assertEqual(self.course.scorm, self.scorm)
         self.assertEqual(self.course.experiences.count(), 1)
+        self.assertEqual(self.course.experiences.first(), self.experience)
 
-    def test_image_upload_path(self):
-        """Проверка пути загрузки изображения"""
-        self.assertTrue(
-            self.course.image.name.startswith('courses/'),
-            msg=f"Неверный путь загрузки изображения: {self.course.image.name}"
+    def test_str_method(self):
+        """Проверка строкового представления"""
+        self.assertEqual(str(self.course), "Basics")
+
+    def test_image_upload(self):
+        """Проверка загрузки изображения"""
+        test_image = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=b'simple image content',
+            content_type='image/jpeg'
         )
-# class CourseModelTest(TestCase):
+        self.course.image = test_image
+        self.course.save()
+        self.assertTrue(self.course.image.name.startswith('courses/'))
+        self.assertIn('test_image', self.course.image.name)
+
+    def test_meta_options(self):
+        """Проверка мета-опций"""
+        self.assertEqual(Course._meta.verbose_name, _("Course"))
+        self.assertEqual(Course._meta.verbose_name_plural, _("Courses"))
+
+
+class CourseAPITestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.profession = Profession.objects.create(verbose_name="Name Profession")
+        cls.experience = WorkExperience.objects.create(years=1)
+        cls.scorm = SCORM.objects.create(title="SCORM")
+
+        cls.course_data = {
+            "name": "specialist",
+            "description": "specialist basics",
+            "beginer": True,
+            "profession": cls.profession.id,
+            "experiences": [cls.experience.id],
+            "scorm": cls.scorm.id
+        }
+
+    def test_create_course(self):
+        """Тест создания курса через API"""
+        url = reverse('course-list')
+        response = self.client.post(url, self.course_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Course.objects.count(), 1)
+
+        course = Course.objects.first()
+        self.assertEqual(course.name, self.course_data['name'])
+        self.assertEqual(course.profession.id, self.course_data['profession'])
+        self.assertEqual(course.experiences.count(), 1)
+
+    def test_retrieve_course(self):
+        """Тест получения данных курса"""
+        course = Course.objects.create(
+            name="Existing Course",
+            description="Test description",
+            profession=self.profession
+        )
+        course.experiences.add(self.experience)
+
+        url = reverse('course-detail', args=[course.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], course.name)
+        self.assertEqual(response.data['profession'], course.profession.id)
+        self.assertEqual(len(response.data['experiences']), 1)
+
+    def test_update_course(self):
+        """Тест обновления курса"""
+        course = Course.objects.create(
+            name="Old Name",
+            description="Old Description",
+            profession=self.profession
+        )
+
+        updated_data = {
+            "name": "Updated Name",
+            "description": "Updated Description",
+            "beginer": False
+        }
+
+        url = reverse('course-detail', args=[course.id])
+        response = self.client.patch(url, updated_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        course.refresh_from_db()
+        self.assertEqual(course.name, updated_data['name'])
+        self.assertEqual(course.beginer, updated_data['beginer'])
+
+    def test_delete_course(self):
+        """Тест удаления курса"""
+        course = Course.objects.create(
+            name="Course to delete",
+            description="Will be deleted",
+            profession=self.profession
+        )
+
+        url = reverse('course-detail', args=[course.id])
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Course.objects.count(), 0)
+
+
+        # class CourseModelTest(TestCase):
     # def setUp(self):
     #     self.profession = Profession.objects.create(name="Test Profession")
     #     self.experience = WorkExperience.objects.create(name="Test Experience")
